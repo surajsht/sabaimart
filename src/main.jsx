7931eff23ea44f9e6efb15602e580999b47ee40c
suraj <surajsht2055@gmail.com>
2025-06-26 17:49:41 +0545

    installed redux-persist package for storing data in local storage

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -8,18 +7,21 @@ import "./output.css";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.jsx";
-import store from "./redux/store.js";
+import store, { persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <ScrollToTop />
-      <QueryClientProvider client={client}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <QueryClientProvider client={client}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
